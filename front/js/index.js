3ded3993be6b03c01a1f83e5a50480406d499788
Alexis Hamoutahra <alexis.hamoutahra@gmail.com>
2022-02-08 12:39:18 +0100

    Affichage des produits sur la page d'accueil, function displayproduct + createElement a +img+h3+p

--- front/js/index.js
+++ front/js/index.js
@@ -1,20 +1,47 @@
-const getProducts = async() => {
-    const response = await fetch("http://localhost:3000/api/products");
-    const data = await response.json();
+const url = "http://localhost:3000/api/products";
 
-    console.log(data);
-    //Boucle produit
-    data.forEach((product) => {
-        console.log(product)
-    });
+const getProducts = async(url) => {
+    const response = await fetch(url);
+    const articles = await response.json();
+
+    articles.forEach((article) => displayProduct(article));
 }
 
-const displayProduct = (product) => {
-    console.log(product);
+const displayProduct = (article) => {
+    console.log(article);
+
+    //Insertion de l'élement "a"
+    let productLink = document.createElement("a");
+    productLink.href = `product.html?id=${article._id}`;
+    document.querySelector(".items").appendChild(productLink);
+
+    // Insertion de l'élément "article"
+    let productArticle = document.createElement("article");
+    productLink.appendChild(productArticle);
+
+    // Insertion de l'image
+    let productImg = document.createElement("img");
+    productImg.src = article.imageUrl;
+    productImg.alt = article.altTxt;
+    productArticle.appendChild(productImg);
+
+    // Insertion du titre "h3"
+    let productName = document.createElement("h3");
+    productName.classList.add("productName");
+    productName.innerHTML = article.name;
+    productArticle.appendChild(productName);
+
+    // Insertion de la description "p"
+    let productDescription = document.createElement("p");
+    productDescription.classList.add("productName");
+    productDescription.innerHTML = article.description;
+    productArticle.appendChild(productDescription);
+};
+
+//const displayProduct =document.getElementById("items").textContent
     // récupérer l'id de la section de l'index.html
     // création html d'un produit
-}
 
 
 
-getProducts();
+getProducts(url);
